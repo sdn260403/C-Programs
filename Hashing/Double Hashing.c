c427911676bbc4d7105da03e6c0a1e14d2b1ab84
Soumyadeep Nandy <108817587+sdn260403@users.noreply.github.com>
2023-04-02 11:32:01 +0530

    Create Double Hashing.c

---
+++ Hashing/Double Hashing.c
@@ -0,0 +1,161 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#define max(a,b) (a>b)?a:b
+inline int hash(int);
+inline int hash_2(int);
+void double_hashing(int);
+void change();
+void del(int);
+int search(int);
+int nxtprime();
+void fill();
+
+struct lp
+{
+int *a,ts,n,m,c;
+}s;
+
+
+int main()
+{
+	s.ts=-1;
+	s.c=0;
+	int ch,key;
+	printf("Enter number of elements: ");
+	scanf("%d",&s.n);
+	s.m=nxtprime(s.n);
+	s.a=(int*)malloc(sizeof(int)*s.m);
+	fill();
+
+	printf("\n1 to insert\n2 to delete\n3 to search\n4 to exit");
+	do
+	{
+		printf("\nEnter Choice\n");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1: printf("ENTER ELEMENT: ");
+					scanf("%d",&key);
+					double_hashing(key);
+					if(s.c>1 && max(s.ts,key)==key)
+					change(key+1);
+					break;
+			
+			case 2: printf("ENTER ELEMENT TO BE DELETED: ");
+					scanf("%d",&key);
+					del(key);
+					break;
+			
+			case 3: printf("ENTER ELEMENT TO BE SEARCHED: ");		
+					scanf("%d",&key);
+					int x=search(key);
+					if(x==s.ts)
+					printf("NOT FOUND");
+					else
+					printf("FOUND AT POSITION %d",(x+1));
+					break;
+			
+		}
+	}while(ch!=4);
+}
+
+void fill()
+{
+	int i=0;
+	while(i<s.m)
+	{
+	s.a[i]=0;
+	i+=1;
+	}
+}
+
+inline int hash(int x)
+{
+	return (x%s.m);
+}
+
+inline int hash_2(int x)
+{
+	return (s.n-(x%s.n));
+}
+
+int nxtprime(int x)
+{
+	int i,flag=1;
+	while(1)
+	{
+		x+=1;
+		for(i=2;i<=ceil(sqrt(x));i++)
+		{
+			if(x%i==0)
+			{
+				flag=0;
+				break;
+			}
+		}
+		if(flag==1)
+			return x;
+		else
+			flag=1;
+	}
+}
+
+void double_hashing(int k)
+{
+	int i,j;
+	if(s.c>=s.m)
+		printf("TABLE IS FULL...CANNOT ENTER ELEMENT");
+	else
+	{
+		i=hash(k);
+		j=hash_2(k);
+		while(s.a[i] != 0 || i>s.m)
+			i=(i+j)%s.m;
+		s.a[i]=k;
+		s.c+=1;
+	}
+	
+}
+
+void change(int x)
+{
+	int i=0;
+	while(i<s.m)
+	{
+		if(s.a[i]==s.ts)
+		s.a[i]=x+1;
+		i+=1;
+	}
+	s.ts=x+1;
+}
+
+void del(int x)
+{
+	int i=search(x);
+	if(i!=s.ts)
+	{
+		s.a[i]=s.ts;
+		s.c-=1;
+	}
+	else
+		printf("ELEMENT TO BE DELETED NOT FOUND"); 
+}
+
+int search(int x)
+{
+	int i,j;
+	i=hash(x);
+	if(s.a[i]==x)
+	return i;
+	i+=1;
+	j=hash(x);
+	while(i!=j)
+	{
+		if(s.a[i]==x)
+			return i;
+		i=(i+1)%s.m;
+	}
+	return s.ts;
+}
+
